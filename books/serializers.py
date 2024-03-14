7ab64d30a1641a7a99cf25148d8a5d8cd01349d4
Khaydarov <@khaydaro1>
2024-03-14 22:58:04 +0500

    Validation

--- books/serializers.py
+++ books/serializers.py
@@ -1,4 +1,6 @@
 from rest_framework import serializers
+from rest_framework.exceptions import ValidationError
+
 from .models import Book
 
 
@@ -6,3 +8,35 @@ class BookSerializer(serializers.ModelSerializer):
     class Meta:
         model = Book
         fields = ('id', 'title', 'author', 'content', 'subtitle', 'isbn', 'price',)
+
+
+    def validate(self, data):
+        title = data.get('title',None)
+        author = data.get('author',None)
+
+
+
+        # alphabetical chars
+        if not title.isalpha():
+            raise ValidationError({
+                'status': False,
+                'message': 'Kitobni sarlavhasi harflardan tashkin topishi kerak'
+            })
+
+
+
+        if Book.objects.filter(title=title, author=author).exists():
+            raise ValidationError(
+                {
+                    'status': False
+                    'message': 'Kitob sarlavhasi va mualifi bir xil bolsa yuklay olmaysiz'
+                }
+            )
+        return data
+
+    def validate_price(self, price):
+        if price <0 or price >99999999999:
+            raise ValidationError({
+                'status': False
+                'message': 'Narx '
+            })
